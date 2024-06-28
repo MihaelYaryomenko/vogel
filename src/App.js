438618e2483d86647afe1883d83d70dd3abef60c
Mihael Yaryomenko <mihaelyaryomenko2005@gmail.com>
2024-06-28 21:02:37 +0700

    final scuratov show

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import portraitLogo from './images/portrait.svg'
 import signoutLogo from './images/sign-out-alt.svg'
 import pagesLogo from './images/window-maximize.svg'
 import flameLogo from './images/flame.svg'
+import { ReactComponent as ProfileLogo } from './images/portrait.svg'
+import { ReactComponent as PlusLogo } from './images/plus.svg'
+import { ReactComponent as ArrowLogo } from './images/arrow-right.svg'
 
 function App() {
   return (
@@ -21,6 +24,29 @@ function App() {
         <SideBarButton icon={pagesLogo} text="pages" />
         <SideBarButton icon={flameLogo} text="trending" />
         <button id="blue-sign-in-btn">{'sign in +'.toUpperCase()}</button>
+      </div>
+      <div id="main-container">
+        <div id="tabs-container">
+          <div id="left-tabs-btn" className="tabs-btn  selected-tabs-btn">
+            <p className="tabs-btn-paragraph">FEED</p>
+            <div id="tabs-btn-border"></div>
+          </div>
+          <div className="tabs-btn">
+            <p className="tabs-btn-paragraph">PEOPLE</p>
+          </div>
+          <div id="right-tabs-btn" className="tabs-btn">
+            <p className="tabs-btn-paragraph">TRENDING</p>
+          </div>
+        </div>
+        <div id="new-post-container">
+          <ProfileLogo className="new-post-icon" style={{ fill: '#0d6efd' }} />
+          <p id="new-post-paragraph">What's on your mind.</p>
+          <PlusLogo className="new-post-icon" style={{ fill: '#0d6efd' }} />
+        </div>
+        <div id="follow-people-container">
+          <p id="follow-people-paragraph">Follow People</p>
+          <ArrowLogo id="follow-people-arrow-icon" style={{ fill: 'black' }} />
+        </div>
       </div>
     </>
   )
